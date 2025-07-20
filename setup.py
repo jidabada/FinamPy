d100e1b802430f32adc23f3f0c578bb1467ae542
jidabada <skippydy@skip.com>
2025-07-20 15:39:42 +0200

    "build: Add setup.py to make the package installable"

---
+++ setup.py
@@ -0,0 +1,12 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='FinamPy',
+    version='0.1.0',
+    packages=find_packages(),
+    description='Custom version of FinamPy gRPC client',
+    author='jidabada',
+    author_email='jidabada@gmail.com'
+)
